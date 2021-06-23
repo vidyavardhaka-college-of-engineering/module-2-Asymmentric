0d8940f990b10afd201955dc4384584e01f28cb0
asymmentric <74967082+Asymmentric@users.noreply.github.com>
2021-06-23 17:53:05 +0000

    Sum of n elements

--- Program-5.c
+++ Program-5.c
@@ -9,8 +9,8 @@
 int main()
 {
    int a[10],i,sum,n;
-   printf("Enter number of elements in array and the array elements");
-   scanf("%d", &n);
+   printf("Enter number of elements in array and the array elements" );
+   scanf("%d", &n); 
  
   // printf("Enter %d elements\n", n);
  
@@ -19,7 +19,7 @@ int main()
       scanf("%d", &a[i]);
       sum=sum+a[i]; 
    }
- printf("Sum of elements=%d", sum);
+ printf("Sum of elements=%d",sum);
  
   return 0;
 }
